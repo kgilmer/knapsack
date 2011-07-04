1c1f2735352be77ec7590b1758023b4d49063fab
Ken Gilmer <kgilmer@gmail.com>
2011-07-04 11:31:17 +0900

    knapsack: add 'force' option to shutdown

--- org/knapsack/shell/BuiltinCommands.java
+++ org/knapsack/shell/BuiltinCommands.java
@@ -343,6 +343,19 @@ public class BuiltinCommands implements IKnapsackCommandSet {
 		private static final String MSG = "OSGi framework is shutting down due to user request via shell.";
 		public String execute() throws Exception {
 			log.log(LogService.LOG_INFO, MSG);
+			//If force option enabled, kill the jvm after 4 seconds regardless if framework shutdown is complete.
+			if (arguments.contains("-f"))
+				(new Thread() {
+					@Override
+					public void run() {
+						try {
+							Thread.sleep(4000);
+							System.exit(0);
+						} catch (InterruptedException e) {
+							
+						}
+					}
+				}).start();
 			context.getBundle(0).stop();
 			
 			return MSG;
@@ -350,6 +363,11 @@ public class BuiltinCommands implements IKnapsackCommandSet {
 
 		public String getName() {
 			return "shutdown-knapsack";
+		}
+		
+		@Override
+		public String getUsage() {			
+			return "-f (force)";
 		}
 
 		public String getDescription() {
